8252f94124ed24ee2c034a9f781b7827885b6949
Vincent Olivier <vincent@up4.com>
2018-11-20 17:47:10 -0500

    Manager : removeLinkedDevices updates isMultiDevice and saves the account Manager : addDevice, getLinkedDevices save the account
    SignalAccount : save/load isMultiDevice
    SignalAccount : save profileKey
    SignalAccount : registrationLockPin doesn't automagically becomes the "null" string, and stays null if null

--- src/main/java/org/asamk/signal/manager/Manager.java
+++ src/main/java/org/asamk/signal/manager/Manager.java
@@ -250,11 +250,15 @@ public class Manager implements Signal {
     public List<DeviceInfo> getLinkedDevices() throws IOException {
         List<DeviceInfo> devices = accountManager.getDevices();
         account.setMultiDevice(devices.size() > 1);
+        account.save();
         return devices;
     }
 
     public void removeLinkedDevices(int deviceId) throws IOException {
         accountManager.removeDevice(deviceId);
+        List<DeviceInfo> devices = accountManager.getDevices();
+        account.setMultiDevice(devices.size() > 1);
+        account.save();
     }
 
     public void addDeviceLink(URI linkUri) throws IOException, InvalidKeyException {
@@ -269,6 +273,7 @@ public class Manager implements Signal {
 
         accountManager.addDevice(deviceIdentifier, deviceKey, identityKeyPair, Optional.of(account.getProfileKey()), verificationCode);
         account.setMultiDevice(true);
+        account.save();
     }
 
     private List<PreKeyRecord> generatePreKeys() {
