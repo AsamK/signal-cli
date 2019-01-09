58895aaf03742cb5c2067ec54ab3ab2bfaaa27b4
Parker Higgins <parkerhiggins@gmail.com>
2019-01-09 14:17:45 -0500

    Expose filename of attachments to json message handler (#185)

--- src/main/java/org/asamk/signal/JsonAttachment.java
+++ src/main/java/org/asamk/signal/JsonAttachment.java
@@ -6,14 +6,19 @@ import org.whispersystems.signalservice.api.messages.SignalServiceAttachmentPoin
 class JsonAttachment {
 
     String contentType;
+    String filename;
     long id;
     int size;
 
     JsonAttachment(SignalServiceAttachment attachment) {
         this.contentType = attachment.getContentType();
+
         final SignalServiceAttachmentPointer pointer = attachment.asPointer();
         if (attachment.isPointer()) {
             this.id = pointer.getId();
+            if (pointer.getFileName().isPresent()) {
+                this.filename = pointer.getFileName().get();
+            }
             if (pointer.getSize().isPresent()) {
                 this.size = pointer.getSize().get();
             }
