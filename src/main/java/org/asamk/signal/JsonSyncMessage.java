4aa71856415df4580747337e9b058e1c4de5c0ec
Gabriel Fernández Valdés <gabriel@raciantau.com>
2019-09-23 11:56:48 -0400

    Added destination field of sync messages to JSON output

--- src/main/java/org/asamk/signal/JsonSyncMessage.java
+++ src/main/java/org/asamk/signal/JsonSyncMessage.java
@@ -2,18 +2,24 @@ package org.asamk.signal;
 
 import org.whispersystems.signalservice.api.messages.multidevice.ReadMessage;
 import org.whispersystems.signalservice.api.messages.multidevice.SignalServiceSyncMessage;
+import org.whispersystems.signalservice.api.messages.multidevice.SentTranscriptMessage;
 
 import java.util.List;
 
 class JsonSyncMessage {
 
     JsonDataMessage sentMessage;
+    String destination;
     List<String> blockedNumbers;
     List<ReadMessage> readMessages;
 
     JsonSyncMessage(SignalServiceSyncMessage syncMessage) {
         if (syncMessage.getSent().isPresent()) {
-            this.sentMessage = new JsonDataMessage(syncMessage.getSent().get().getMessage());
+            final SentTranscriptMessage sentTranscriptMessage = syncMessage.getSent().get();
+            if (sentTranscriptMessage.getDestination().isPresent()) {
+                this.destination = sentTranscriptMessage.getDestination().get();
+            }
+            this.sentMessage = new JsonDataMessage(sentTranscriptMessage.getMessage());
         }
         if (syncMessage.getBlockedList().isPresent()) {
             this.blockedNumbers = syncMessage.getBlockedList().get().getNumbers();
