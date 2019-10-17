d993a7ae45a0aaf5e7adc265bfb17d3dfcb0712a
Gabriel Fernández Valdés <gabriel@raciantau.com>
2019-10-17 19:58:59 -0400

    added sync type to sync messages json

--- src/main/java/org/asamk/signal/JsonSyncMessage.java
+++ src/main/java/org/asamk/signal/JsonSyncMessage.java
@@ -5,21 +5,36 @@ import org.whispersystems.signalservice.api.messages.multidevice.SignalServiceSy
 
 import java.util.List;
 
+enum JsonSyncMessageType {
+  CONTACTS_SYNC,
+  GROUPS_SYNC,
+  REQUEST_SYNC
+}
+
 class JsonSyncMessage {
 
     JsonSyncDataMessage sentMessage;
     List<String> blockedNumbers;
     List<ReadMessage> readMessages;
+    JsonSyncMessageType type;
 
     JsonSyncMessage(SignalServiceSyncMessage syncMessage) {
-        if (syncMessage.getSent().isPresent()) {
-            this.sentMessage = new JsonSyncDataMessage(syncMessage.getSent().get());
-        }
-        if (syncMessage.getBlockedList().isPresent()) {
-            this.blockedNumbers = syncMessage.getBlockedList().get().getNumbers();
-        }
-        if (syncMessage.getRead().isPresent()) {
-            this.readMessages = syncMessage.getRead().get();
-        }
-    }
+       if (syncMessage.getSent().isPresent()) {
+           this.sentMessage = new JsonSyncDataMessage(syncMessage.getSent().get());
+       }
+       if (syncMessage.getBlockedList().isPresent()) {
+           this.blockedNumbers = syncMessage.getBlockedList().get().getNumbers();
+       }
+       if (syncMessage.getRead().isPresent()) {
+           this.readMessages = syncMessage.getRead().get();
+       }
+
+       if (syncMessage.getContacts().isPresent()) {
+           this.type = JsonSyncMessageType.CONTACTS_SYNC;
+       } else if (syncMessage.getGroups().isPresent()) {
+           this.type = JsonSyncMessageType.GROUPS_SYNC;
+       } else if (syncMessage.getRequest().isPresent()) {
+           this.type = JsonSyncMessageType.REQUEST_SYNC;
+       }
+   }
 }
