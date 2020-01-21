4bafc7212ae24d62c5f2d37514180179dabb139c
AsamK <asamk@gmx.de>
2020-01-21 20:32:19 +0100

    Update copyright notice

--- src/main/java/org/asamk/signal/manager/Manager.java
+++ src/main/java/org/asamk/signal/manager/Manager.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2015-2018 AsamK
+  Copyright (C) 2015-2020 AsamK and contributors
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
