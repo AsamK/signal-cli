d1a00d5101abffdf2b491ef484e3eda23414602e
Carl Bingel <kalle.bingel@gmail.com>
2020-02-09 20:45:34 +0100

    added static member attachmentStoragePath added static setter for attachmentStoragePath added new member storagePath to enable output of full path to the stored attachment on disk

--- src/main/java/org/asamk/signal/JsonAttachment.java
+++ src/main/java/org/asamk/signal/JsonAttachment.java
@@ -1,15 +1,30 @@
 package org.asamk.signal;
 
+import java.io.File;
+
 import org.whispersystems.signalservice.api.messages.SignalServiceAttachment;
 import org.whispersystems.signalservice.api.messages.SignalServiceAttachmentPointer;
 
-class JsonAttachment {
+public class JsonAttachment {
 
     String contentType;
     String filename;
     String id;
+    String storagePath;
     int size;
 
+    // This is a bit of a kludge, but the alternative of getting a reference
+    // to Manager (who knows about the attachmentPath) from somewhere was even worse.
+    static private String attachmentStoragePath = "";
+
+    static public String getAttachmentStoragePath() {
+    	return attachmentStoragePath;
+    }
+    
+    static public void setAttachmentStoragePath( String p) {
+    	attachmentStoragePath = p;
+    }
+    
     JsonAttachment(SignalServiceAttachment attachment) {
         this.contentType = attachment.getContentType();
 
@@ -22,6 +37,7 @@ class JsonAttachment {
             if (pointer.getSize().isPresent()) {
                 this.size = pointer.getSize().get();
             }
+            this.storagePath = attachmentStoragePath + File.separator + this.id;
         }
     }
 }
