44851887894ed9162006ad9b12309e732aa3e876
AsamK <asamk@gmx.de>
2020-11-22 10:48:42 +0100

    Adapt json output to always use receiptMessage and remove isReceipt field
    Fixes #346

--- src/main/java/org/asamk/signal/json/JsonReceiptMessage.java
+++ src/main/java/org/asamk/signal/json/JsonReceiptMessage.java
@@ -22,4 +22,15 @@ class JsonReceiptMessage {
         }
         this.timestamps = receiptMessage.getTimestamps();
     }
+
+    private JsonReceiptMessage(final long when, final boolean isDelivery, final boolean isRead, final List<Long> timestamps) {
+        this.when = when;
+        this.isDelivery = isDelivery;
+        this.isRead = isRead;
+        this.timestamps = timestamps;
+    }
+
+    static JsonReceiptMessage deliveryReceipt(final long when, final List<Long> timestamps) {
+        return new JsonReceiptMessage(when, true, false, timestamps);
+    }
 }
