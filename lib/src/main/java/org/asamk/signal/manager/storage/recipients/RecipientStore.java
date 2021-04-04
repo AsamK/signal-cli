3ad3b2c966102ea26cc80cab3d4107a13f2fcc87
AsamK <asamk@gmx.de>
2021-04-04 09:13:52 +0200

    Move RecipientStore to recipients package

--- lib/src/main/java/org/asamk/signal/manager/storage/recipients/RecipientStore.java
+++ lib/src/main/java/org/asamk/signal/manager/storage/recipients/RecipientStore.java
@@ -1,4 +1,4 @@
-package org.asamk.signal.manager.storage.protocol;
+package org.asamk.signal.manager.storage.recipients;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonGenerator;
