11b3758416ea395d04b8538a28be07c240749767
JtheSaw <21310929+JtheSaw@users.noreply.github.com>
2021-09-13 17:01:26 +0200

    Add sendTyping and sendReceipt to dbus interface (#718)
    * Add sendTyping and sendReceipt to dbus interface
    
    * Resolve requested changes
    
    * Adapt documentation

--- src/main/java/org/asamk/Signal.java
+++ src/main/java/org/asamk/Signal.java
@@ -21,6 +21,14 @@ public interface Signal extends DBusInterface {
             String message, List<String> attachments, List<String> recipients
     ) throws Error.AttachmentInvalid, Error.Failure, Error.InvalidNumber, Error.UntrustedIdentity;
 
+    void sendTyping(
+            String recipient, boolean stop
+    ) throws Error.Failure, Error.GroupNotFound, Error.UntrustedIdentity;
+
+    void sendReadReceipt(
+            String recipient, List<Long> targetSentTimestamp
+    ) throws Error.Failure, Error.UntrustedIdentity;
+
     long sendRemoteDeleteMessage(
             long targetSentTimestamp, String recipient
     ) throws Error.Failure, Error.InvalidNumber;
