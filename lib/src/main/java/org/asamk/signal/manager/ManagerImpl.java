7b2b258b1d9a3d9264c6da4458120048df8b8892
AsamK <asamk@gmx.de>
2022-01-16 10:45:36 +0100

    Update year

--- lib/src/main/java/org/asamk/signal/manager/ManagerImpl.java
+++ lib/src/main/java/org/asamk/signal/manager/ManagerImpl.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2015-2021 AsamK and contributors
+  Copyright (C) 2015-2022 AsamK and contributors
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
