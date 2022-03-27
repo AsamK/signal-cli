b5d578e483abeda7bb5683b674f3fba17fb279bc
AsamK <asamk@gmx.de>
2022-03-27 18:56:36 +0200

    Update graalvm build tools plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     application
     eclipse
     `check-lib-versions`
-    id("org.graalvm.buildtools.native") version "0.9.10"
+    id("org.graalvm.buildtools.native") version "0.9.11"
 }
 
 version = "0.10.4.2"
