6893e91190192779130488c2c0fa05605ebb1ee6
AsamK <asamk@gmx.de>
2022-04-11 22:24:48 +0200

    Update dependencies

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -15,10 +15,10 @@ repositories {
 
 dependencies {
     implementation("com.github.turasa", "signal-service-java", "2.15.3_unofficial_46")
-    implementation("com.fasterxml.jackson.core", "jackson-databind", "2.13.1")
+    implementation("com.fasterxml.jackson.core", "jackson-databind", "2.13.2.2")
     implementation("com.google.protobuf", "protobuf-javalite", "3.11.4")
     implementation("org.bouncycastle", "bcprov-jdk15on", "1.70")
-    implementation("org.slf4j", "slf4j-api", "1.7.32")
+    implementation("org.slf4j", "slf4j-api", "1.7.36")
     implementation("org.xerial", "sqlite-jdbc", "3.36.0.3")
     implementation("com.zaxxer", "HikariCP", "5.0.1")
 }
