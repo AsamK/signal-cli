0c74338c9caeb39cdb378eb61b5f0570e06684e0
technillogue <technillogue@gmail.com>
2022-10-06 17:57:14 +0200

    Bump user agent version
    Closes #1024

--- src/main/java/org/asamk/signal/BaseConfig.java
+++ src/main/java/org/asamk/signal/BaseConfig.java
@@ -5,7 +5,7 @@ public class BaseConfig {
     public final static String PROJECT_NAME = BaseConfig.class.getPackage().getImplementationTitle();
     public final static String PROJECT_VERSION = BaseConfig.class.getPackage().getImplementationVersion();
 
-    final static String USER_AGENT_SIGNAL_ANDROID = "Signal-Android/5.51.7";
+    final static String USER_AGENT_SIGNAL_ANDROID = "Signal-Android/5.52.1";
     final static String USER_AGENT_SIGNAL_CLI = PROJECT_NAME == null
             ? "signal-cli"
             : PROJECT_NAME + "/" + PROJECT_VERSION;
