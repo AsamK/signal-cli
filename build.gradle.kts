e4a4788d5efde9c68c2d5464adddde4c94dd81a8
AsamK <asamk@gmx.de>
2022-10-19 20:09:03 +0200

    Add native graalvm build to release

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     application
     eclipse
     `check-lib-versions`
-    id("org.graalvm.buildtools.native") version "0.9.15"
+    id("org.graalvm.buildtools.native") version "0.9.16"
 }
 
 version = "0.11.3"
