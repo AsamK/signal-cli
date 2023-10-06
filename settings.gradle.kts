04de0010b57b4d5ab5cdda5cb79507cf7c7dbabe
AsamK <asamk@gmx.de>
2023-10-06 19:58:09 +0200

    Update libsignal-service-java

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ dependencyResolutionManagement {
             library("logback", "ch.qos.logback", "logback-classic").version("1.4.11")
 
 
-            library("signalservice", "com.github.turasa", "signal-service-java").version("2.15.3_unofficial_83")
+            library("signalservice", "com.github.turasa", "signal-service-java").version("2.15.3_unofficial_84")
             library("sqlite", "org.xerial", "sqlite-jdbc").version("3.43.0.0")
             library("hikari", "com.zaxxer", "HikariCP").version("5.0.1")
             library("junit", "org.junit.jupiter", "junit-jupiter").version("5.10.0")
