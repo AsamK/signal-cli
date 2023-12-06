d3eef6c820e057a4a0abd0a64c6d652a33485e19
AsamK <asamk@gmx.de>
2023-12-06 18:12:17 +0100

    Update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,11 +13,11 @@ dependencyResolutionManagement {
             version("slf4j", "2.0.9")
             library("slf4j.api", "org.slf4j", "slf4j-api").versionRef("slf4j")
             library("slf4j.jul", "org.slf4j", "jul-to-slf4j").versionRef("slf4j")
-            library("logback", "ch.qos.logback", "logback-classic").version("1.4.11")
+            library("logback", "ch.qos.logback", "logback-classic").version("1.4.14")
 
 
             library("signalservice", "com.github.turasa", "signal-service-java").version("2.15.3_unofficial_88")
-            library("sqlite", "org.xerial", "sqlite-jdbc").version("3.44.0.0")
+            library("sqlite", "org.xerial", "sqlite-jdbc").version("3.44.1.0")
             library("hikari", "com.zaxxer", "HikariCP").version("5.1.0")
             library("junit.jupiter", "org.junit.jupiter", "junit-jupiter").version("5.10.1")
             library("junit.launcher", "org.junit.platform", "junit-platform-launcher").version("1.10.1")
