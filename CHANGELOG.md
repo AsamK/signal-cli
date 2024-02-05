3ce9be516694f5cc4a54b700ac668f2f63bb014e
AsamK <asamk@gmx.de>
2024-02-06 00:21:32 +0100

    Bump version to 0.12.8

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## [0.12.8] - 2024-02-06
+
+### Fixes
+- Update user agent
+
 ## [0.12.7] - 2023-12-15
 
 ### Fixes
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("org.graalvm.buildtools.native") version "0.9.28"
 }
 
-version = "0.12.7"
+version = "0.12.8"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
