7206b4da258b5a9b17d3e2c1bc9cee354e9170e9
AsamK <asamk@gmx.de>
2024-02-18 20:48:01 +0100

    Fix use of aci instead of serviceId
    Fixes #1455

--- lib/src/main/java/org/asamk/signal/manager/storage/recipients/RecipientAddress.java
+++ lib/src/main/java/org/asamk/signal/manager/storage/recipients/RecipientAddress.java
@@ -165,11 +165,11 @@ public record RecipientAddress(
     }
 
     public SignalServiceAddress toSignalServiceAddress() {
-        return new SignalServiceAddress(aci.orElse(ACI.UNKNOWN), number);
+        return new SignalServiceAddress(serviceId().orElse(ACI.UNKNOWN), number);
     }
 
     public org.asamk.signal.manager.api.RecipientAddress toApiRecipientAddress() {
-        return new org.asamk.signal.manager.api.RecipientAddress(aci().map(ServiceId::getRawUuid),
+        return new org.asamk.signal.manager.api.RecipientAddress(serviceId().map(ServiceId::getRawUuid),
                 number(),
                 username());
     }
