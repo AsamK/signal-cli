08ba774b7135f83bf50d513214f305a4b0db2181
AsamK <asamk@gmx.de>
2024-02-20 17:37:59 +0100

    Update graalvm buildtools

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     application
     eclipse
     `check-lib-versions`
-    id("org.graalvm.buildtools.native") version "0.10.0"
+    id("org.graalvm.buildtools.native") version "0.10.1"
 }
 
 version = "0.13.1-SNAPSHOT"
