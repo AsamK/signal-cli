6d764db11418d6e3ea090a7c0dafb5ab3cfd6962
AsamK <asamk@gmx.de>
2024-06-02 13:26:17 +0200

    Update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
             library("bouncycastle", "org.bouncycastle", "bcprov-jdk18on").version("1.78.1")
-            library("jackson.databind", "com.fasterxml.jackson.core", "jackson-databind").version("2.17.0")
+            library("jackson.databind", "com.fasterxml.jackson.core", "jackson-databind").version("2.17.1")
             library("argparse4j", "net.sourceforge.argparse4j", "argparse4j").version("0.9.0")
             library("dbusjava", "com.github.hypfvieh", "dbus-java-transport-native-unixsocket").version("5.0.0")
             version("slf4j", "2.0.13")
@@ -16,7 +16,7 @@ dependencyResolutionManagement {
             library("logback", "ch.qos.logback", "logback-classic").version("1.5.6")
 
             library("signalservice", "com.github.turasa", "signal-service-java").version("2.15.3_unofficial_102")
-            library("sqlite", "org.xerial", "sqlite-jdbc").version("3.45.3.0")
+            library("sqlite", "org.xerial", "sqlite-jdbc").version("3.46.0.0")
             library("hikari", "com.zaxxer", "HikariCP").version("5.1.0")
             library("junit.jupiter", "org.junit.jupiter", "junit-jupiter").version("5.10.2")
             library("junit.launcher", "org.junit.platform", "junit-platform-launcher").version("1.10.2")
