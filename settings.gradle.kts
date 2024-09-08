d852c60c373b502f5577ec0b5f6343670ad77c0e
AsamK <asamk@gmx.de>
2024-09-08 18:55:23 +0200

    Update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,17 +9,17 @@ dependencyResolutionManagement {
             library("bouncycastle", "org.bouncycastle", "bcprov-jdk18on").version("1.78.1")
             library("jackson.databind", "com.fasterxml.jackson.core", "jackson-databind").version("2.17.2")
             library("argparse4j", "net.sourceforge.argparse4j", "argparse4j").version("0.9.0")
-            library("dbusjava", "com.github.hypfvieh", "dbus-java-transport-native-unixsocket").version("5.0.0")
+            library("dbusjava", "com.github.hypfvieh", "dbus-java-transport-native-unixsocket").version("5.1.0")
             version("slf4j", "2.0.13")
             library("slf4j.api", "org.slf4j", "slf4j-api").versionRef("slf4j")
             library("slf4j.jul", "org.slf4j", "jul-to-slf4j").versionRef("slf4j")
-            library("logback", "ch.qos.logback", "logback-classic").version("1.5.6")
+            library("logback", "ch.qos.logback", "logback-classic").version("1.5.8")
 
             library("signalservice", "com.github.turasa", "signal-service-java").version("2.15.3_unofficial_107")
-            library("sqlite", "org.xerial", "sqlite-jdbc").version("3.46.0.0")
+            library("sqlite", "org.xerial", "sqlite-jdbc").version("3.46.1.0")
             library("hikari", "com.zaxxer", "HikariCP").version("5.1.0")
-            library("junit.jupiter", "org.junit.jupiter", "junit-jupiter").version("5.10.2")
-            library("junit.launcher", "org.junit.platform", "junit-platform-launcher").version("1.10.2")
+            library("junit.jupiter", "org.junit.jupiter", "junit-jupiter").version("5.11.0")
+            library("junit.launcher", "org.junit.platform", "junit-platform-launcher").version("1.11.0")
         }
     }
 }
