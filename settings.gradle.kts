bab8ddf35ad9fa3641e3a731b82665d70e1ba0e0
AsamK <asamk@gmx.de>
2024-09-09 18:31:47 +0200

    Update slf4j

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ dependencyResolutionManagement {
             library("jackson.databind", "com.fasterxml.jackson.core", "jackson-databind").version("2.17.2")
             library("argparse4j", "net.sourceforge.argparse4j", "argparse4j").version("0.9.0")
             library("dbusjava", "com.github.hypfvieh", "dbus-java-transport-native-unixsocket").version("5.0.0")
-            version("slf4j", "2.0.13")
+            version("slf4j", "2.0.16")
             library("slf4j.api", "org.slf4j", "slf4j-api").versionRef("slf4j")
             library("slf4j.jul", "org.slf4j", "jul-to-slf4j").versionRef("slf4j")
             library("logback", "ch.qos.logback", "logback-classic").version("1.5.8")
