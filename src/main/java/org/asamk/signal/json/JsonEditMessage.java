9db116cab2a98db8a39bc3af35721faa43c22994
Jailson Dias <jaison.diasc@gmail.com>
2024-10-24 17:27:51 -0300

    add group info on json message

--- src/main/java/org/asamk/signal/json/JsonEditMessage.java
+++ src/main/java/org/asamk/signal/json/JsonEditMessage.java
@@ -1,10 +1,11 @@
 package org.asamk.signal.json;
 
+import org.asamk.signal.manager.Manager;
 import org.asamk.signal.manager.api.MessageEnvelope;
 
 record JsonEditMessage(long targetSentTimestamp, JsonDataMessage dataMessage) {
 
-    static JsonEditMessage from(MessageEnvelope.Edit editMessage) {
-        return new JsonEditMessage(editMessage.targetSentTimestamp(), JsonDataMessage.from(editMessage.dataMessage()));
+    static JsonEditMessage from(MessageEnvelope.Edit editMessage, Manager m) {
+        return new JsonEditMessage(editMessage.targetSentTimestamp(), JsonDataMessage.from(editMessage.dataMessage(), m));
     }
 }
