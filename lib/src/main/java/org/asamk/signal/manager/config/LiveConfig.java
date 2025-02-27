cf5c943127e90c6168c4ea54cb57b449cc93f71a
AsamK <asamk@gmx.de>
2025-02-27 11:34:23 +0100

    Remove legacy SVR2 enclave

--- lib/src/main/java/org/asamk/signal/manager/config/LiveConfig.java
+++ lib/src/main/java/org/asamk/signal/manager/config/LiveConfig.java
@@ -29,7 +29,6 @@ class LiveConfig {
             .decode("BXu6QIKVz5MA8gstzfOgRQGqyLqOwNKHL6INkv3IHWMF");
     private static final String CDSI_MRENCLAVE = "0f6fd79cdfdaa5b2e6337f534d3baf999318b0c462a7ac1f41297a3e4b424a57";
     private static final String SVR2_MRENCLAVE = "9314436a9a144992bb3680770ea5fd7934a7ffd29257844a33763a238903d570";
-    private static final String SVR2_LEGACY_MRENCLAVE = "a6622ad4656e1abcd0bc0ff17c229477747d2ded0495c4ebee7ed35c1789fa97";
 
     private static final String URL = "https://chat.signal.org";
     private static final String CDN_URL = "https://cdn.signal.org";
@@ -89,7 +88,7 @@ class LiveConfig {
                 createDefaultServiceConfiguration(interceptors),
                 getUnidentifiedSenderTrustRoot(),
                 CDSI_MRENCLAVE,
-                List.of(SVR2_MRENCLAVE, SVR2_LEGACY_MRENCLAVE));
+                List.of(SVR2_MRENCLAVE));
     }
 
     private LiveConfig() {
