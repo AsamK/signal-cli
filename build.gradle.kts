9458972d15d51c0b0c1274a57d23b8398f6dbc29
AsamK <asamk@gmx.de>
2026-01-24 11:38:01 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     application
     eclipse
     `check-lib-versions`
-    id("org.graalvm.buildtools.native") version "0.11.3"
+    id("org.graalvm.buildtools.native") version "0.11.4"
 }
 
 allprojects {
