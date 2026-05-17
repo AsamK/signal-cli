bacced07730403236ce362513ada29d6219b2264
Stefan Meinecke <meinecke@greensec.de>
2026-05-17 14:32:37 +0200

    Add missing unidentified keep-alive methods to test stub
    Implement addUnidentifiedKeepAlive and removeUnidentifiedKeepAlive in SseInitialFlushTest's Manager stub to match the updated interface.

--- src/test/java/org/asamk/signal/http/SseInitialFlushTest.java
+++ src/test/java/org/asamk/signal/http/SseInitialFlushTest.java
@@ -448,6 +448,14 @@ class SseInitialFlushTest {
         }
 
         @Override
+        public void addUnidentifiedKeepAlive(String token) {
+        }
+
+        @Override
+        public void removeUnidentifiedKeepAlive(String token) {
+        }
+
+        @Override
         public InputStream retrieveAttachment(String id) {
             return null;
         }
